7ac5f0d587e721e874769fed88aeb71e8b93b0ff
reinhosas <sascha-reinhold@web.de>
2015-09-10 19:09:21 +0200

    kacke

---
+++ Ruby/code/hello_world.rb
@@ -0,0 +1,12 @@
+
+var = "Hello Ruby"
+var2 = "heute ist der 9.9.2015"
+var3 = 1
+puts var + " " + var2 + var3.to_s
+
+puts "#{var1} #{Time.new} #{var3}"
+
+#Variable als String deklarieren.
+	var = "" #geht mit und ohne Inhalt.
+	oder
+	var = String.new
